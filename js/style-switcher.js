b5272156e81510a3d76b5b06a49b8c09854cf895
Vishal <Vishalarya2468@gmail.com>
2025-05-10 22:39:19 +0530

    profile pic design updated

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -34,9 +34,9 @@ dayNight.addEventListener('click',()=>{
 
 window.addEventListener('load', ()=>{
     if( document.body.classList.contains("light")){
-        dayNight.querySelector("i").classList.add("fa-moon")
-    }else{
         dayNight.querySelector("i").classList.add("fa-sun")
+    }else{
+        dayNight.querySelector("i").classList.add("fa-moon")
     }
 })
 
